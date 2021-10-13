bd3174b62888407680a2356ed05ff2ff98fcbe69
jed-chang <cha19057@byui.edu>
2021-10-13 11:18:29 -0600

    Add director class

---
+++ 05-jumper/jumper/game/director.py
@@ -0,0 +1,25 @@
+#from game.board import Board
+#from game.word import Word
+#from game.guess import Guess
+
+class Director:
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        #self.board = Board()
+        #self.word = Word()
+        self.keep_playing = True
+        #self.guess = Guess()
+
+    def start_game(self):
+        while self.keep_playing:
+            pass
+
+    def get_input(self):
+        pass
+
+    def do_output(self):
+        pass
