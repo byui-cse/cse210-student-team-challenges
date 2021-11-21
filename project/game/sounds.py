cc6d6d778677089461ea813d89f4e6f0c7e597ee
davidjem <davidjulian.es@gmail.com>
2021-11-20 22:20:29 -0500

    added sound to keys

--- project/game/sounds.py
+++ project/game/sounds.py
@@ -1,11 +1,14 @@
 import arcade 
 class Sounds:
-    # def load_sound():
-    #     arcade.load_sound('project\game\sounds\quick-jump.wav')
+    def __init__(self):
+        self.sound_file = 'project\game\sounds\quick-jump.wav'
+        self.sound = arcade.Sound(self.sound_file)
+    def load_sound(self):
+        arcade.load_sound(self.sound_file)
 
-    def play_sound():
-        arcade.load_sound('project\game\sounds\quick-jump.wav')
+    def play_sound(self):
+        self.sound.play(1, 0, False)
         arcade.play_sound(1, 0, False)  
 
-    def stop_sound():
+    def stop_sound(self):
         arcade.stop_sound()
