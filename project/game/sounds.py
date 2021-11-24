6a4e08b9fde9f734b11fdd8a46566ae47e131378
davidjem <davidjulian.es@gmail.com>
2021-11-24 07:14:18 -0500

    changed the name of some variables for better understanding

--- project/game/sounds.py
+++ project/game/sounds.py
@@ -1,10 +1,10 @@
 import arcade 
 class Sounds:
     def __init__(self):
-        self.sound_file = ':resources:sounds/jump2.wav'
-        self.sound = arcade.Sound(self.sound_file)
+        self.jump_sound_file = ':resources:sounds/jump2.wav'
+        self.sound = arcade.Sound(self.jump_sound_file)
     def load_sound(self):
-        arcade.load_sound(self.sound_file)
+        arcade.load_sound(self.jump_sound_file)
 
     def play_sound(self):
         self.sound.play(1, 0, False)
